e85f555e703811b71c34bf8ca7d0603e67adc363
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-09-20 00:57:38 +0500

    Add files via upload

---
+++ Новая папка/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Car
+{
+    int power;
+    int price;
+    int brand; 
+
+    public Car(int power, int price, int brand){
+        this.power = power;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    public int getPrice(){
+        return power * brand;
+    } 
+
+}
+    public class Simple{
+        public static void Main(){
+            Car firstCar = new Car(10, 2, 5);
+            Console.WriteLine(firstCar.getPrice());
+        }
+}
+
+        
+
+
+
+
+
+
+
+
+
+
